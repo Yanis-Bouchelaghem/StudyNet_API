ac1491c729fa1f44672ae62e0dc4ffd4595bd925
Alphashow <lastblast@outlook.fr>
2021-04-15 23:24:20 +0100

    Added department model.

--- Management/models.py
+++ Management/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.utils.translation import ugettext_lazy as _
 # Create your models here.
+
+class Department(models.Model):
+    """
+        Represents a department with a code and a name.
+    """
+    code = models.CharField(max_length=30,primary_key=True,blank=False,
+        help_text=_('A code that uniquely identifies this section.'))
+    name = models.CharField(max_length=80,blank=False,
+        help_text=_('Human friendly name.'))
