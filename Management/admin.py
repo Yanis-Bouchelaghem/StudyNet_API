168215578b3c9cd58257c819884e3d4ab60be2e8
Alphashow <lastblast@outlook.fr>
2021-04-16 00:29:56 +0100

    Registered models to admin.

--- Management/admin.py
+++ Management/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Department,Specialty,Section
 
 # Register your models here.
+
+admin.site.register(Department)
+admin.site.register(Specialty)
+admin.site.register(Section)
