b07fb1f9f84cdc725e6b5774bbfc89a5e92fb019
Alphashow <lastblast@outlook.fr>
2021-04-15 16:22:10 +0100

    Added the user model.

--- Accounts/models.py
+++ Accounts/models.py
@@ -1,3 +1,61 @@
 from django.db import models
-
+from django.contrib.auth.models import BaseUserManager
+from django.utils.translation import ugettext_lazy as _
 # Create your models here.
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """
+    A fully featured User model with admin-compliant permissions that uses
+    a full-length email field as the username.
+
+    Email, first_name, last_name and password are required. Other fields are optional.
+    """
+    class Types(models.TextChoices):
+        Superuser = "SUPERUSER",_("Superuser")
+        Administrator = "ADMINISTRATOR",_("Administator")
+        Teacher = "TEACHER",_("Teacher")
+        Student = "STUDENT",_("Student")
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(_('email address'), max_length=254, unique=True)
+    first_name = models.CharField(_('first name'), max_length=30, blank=False, null=False)
+    last_name = models.CharField(_('last name'), max_length=30, blank=False,null=False)
+    user_type = models.CharField(max_length=40,choices=Types.choices,default=Types.Student,
+        help_text=_('Determines the type of user this is.'))
+    is_staff = models.BooleanField(_('staff status'), default=False,
+        help_text=_('Designates whether the user can log into this admin '
+                    'site.'))
+    is_active = models.BooleanField(_('active'), default=True,
+        help_text=_('Designates whether this user should be treated as '
+                    'active. Unselect this instead of deleting accounts.'))
+    is_complete = models.BooleanField(_('is complete'),default=True,
+        help_text=_('Designates whether this user\'s data is complete.'))
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name','last_name']
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def get_absolute_url(self):
+        return "/users/%s/" % urlquote(self.email)
+
+    def get_full_name(self):
+        """
+        Returns the last_name plus the first_name , with a space in between.
+        """
+        full_name = '%s %s' % (self.last_name, self.first_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        "Returns the short name for the user."
+        return self.last_name
+
+    def email_user(self, subject, message, from_email=None):
+        """
+        Sends an email to this User.
+        """
+        send_mail(subject, message, from_email, [self.email])
