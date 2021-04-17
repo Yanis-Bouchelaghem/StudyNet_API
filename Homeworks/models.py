6c819edfcfcf4d9b6562773aa3604a3411329856
Alphashow <lastblast@outlook.fr>
2021-04-17 18:08:06 +0100

    Added homework model.

--- Homeworks/models.py
+++ Homeworks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.postgres.fields import ArrayField
+from django.utils.translation import ugettext_lazy as _
 # Create your models here.
+
+class Homework(models.Model):
+    """
+        Represents a homework with 
+    """
+    id = models.BigAutoField(_("id"), primary_key=True)
+    assignment = models.ForeignKey("Management.Assignment", verbose_name=_("assignment"), on_delete=models.CASCADE)
+    concerned_groups = ArrayField(base_field=models.PositiveSmallIntegerField())
+    title = models.CharField(_("title"), max_length=150)
+    due_date = models.DateTimeField(_("due date"), auto_now=False, auto_now_add=False)
+    comment = models.TextField(_("comment"))
--- StudyNet/settings.py
+++ StudyNet/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'Accounts',
     'Management',
     'Sessions',
+    'Homeworks',
 ]
 
 MIDDLEWARE = [
