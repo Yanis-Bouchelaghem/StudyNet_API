9c04444f69529bd612864c3f3596977c5dc4d205
Alphashow <lastblast@outlook.fr>
2021-04-20 18:08:14 +0100

    Implemented student list retrieval.

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User,Teacher
+from .models import User,Teacher,Student
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Teacher)
+admin.site.register(Student)
--- Accounts/views.py
+++ Accounts/views.py
@@ -1,3 +1,18 @@
+#View related imports
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+#Custom imports
+from .models import Student
+from .serializers import CreateStudentSerializer
 
 # Create your views here.
+class StudentList(APIView):
+    #TODO : Require authentication when done with login view (uncomment below).
+    #permission_classes=[IsAuthenticated]
+    def get(self,request):
+        students = Student.objects.all()
+        serializer = CreateStudentSerializer(students,many=True)
+        return Response(serializer.data)
