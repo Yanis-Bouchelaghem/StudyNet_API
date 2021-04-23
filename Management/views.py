bb8268c9327fe773caa21aba680db92de87828c7
Alphashow <lastblast@outlook.fr>
2021-04-23 22:20:14 +0100

    Specialties can be filtered by department, sections by specialty.

--- Management/views.py
+++ Management/views.py
@@ -21,21 +21,35 @@ class DepartmentList(APIView):
 class SpecialtyList(APIView):
     """
         Retrieves the list of specialties.
+        Can be filtered by department.
     """
     permission_classes = []
 
+    def get_queryset(self):
+        department = self.request.query_params.get('department',None)
+        if department:
+            return Specialty.objects.filter(Department=department)
+        return Specialty.objects.all()
+    
     def get(self,request):
-        specialties = Specialty.objects.all()
+        specialties = self.get_queryset()
         serializer = SpecialtySerializer(specialties, many=True)
         return Response(serializer.data)
 
 class SectionList(APIView):
     """
         Retrieves the list of sections.
+        Can be filtered by specialty.
     """
     permission_classes = []
 
+    def get_queryset(self):
+        specialty = self.request.query_params.get('specialty',None)
+        if specialty:
+            return Section.objects.filter(specialty=specialty)
+        return Section.objects.all()
+
     def get(self,request):
-        sections = Section.objects.all()
+        sections = self.get_queryset()
         serializer = SectionSerializer(sections, many=True)
         return Response(serializer.data)
