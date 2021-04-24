16e92a4bf78424fb04e0257997b17496a127dfaa
Alphashow <lastblast@outlook.fr>
2021-04-24 16:50:23 +0100

    Heavy optimisations done to previous unit tests + implemented logout unit tests.

--- Accounts/tests/test_setup.py
+++ Accounts/tests/test_setup.py
@@ -37,32 +37,60 @@ class TestSetup(APITestCase):
                 self.section2.code
             ]
         }
-        #Create an admin user and a token.
-        self.admin_user = User.objects.create_user(email='admin@me.com',first_name='admin',last_name='admin',
-        password='adminpass',user_type=User.Types.ADMINISTRATOR)
-        self.admin_user_token = AuthToken.objects.create(user=self.admin_user)
-        
-        #Create a teacher user, their account and a token.
-        self.teacher_user = User.objects.create_user(email='teacher@me.com',first_name='teacher',last_name='teacher',
-        password='teacherpass',user_type=User.Types.TEACHER)
-        self.teacher_user_token = AuthToken.objects.create(user=self.teacher_user)
-        self.teacher_account = Teacher.objects.create(user=self.teacher_user,grade='MAB')
-        
-        #Create a student user, their account and a token.
-        self.student_user = User.objects.create_user(email='student@me.com',first_name='student',last_name='student',
-        password='studentpass',user_type=User.Types.STUDENT)
-        self.student_user_token = AuthToken.objects.create(user=self.student_user)
-        self.student_account = Student.objects.create(user=self.student_user,section=self.section1,
-        registration_number='151638468951',group=1)
-
-        #Create a superuser and a token.
-        self.super_user = User.objects.create_superuser(email='superuser@me.com',first_name='superuser',last_name='superuser',
-        password='superuserpass')
-        self.super_user_token = AuthToken.objects.create(user=self.super_user)
         return super().setUp()
 
-        
+    def create_dummy_admin(self):
+        """
+        returns a dictionary containing the admin user data and the token.
+        """
+        admin_user = User.objects.create_user(email='admin@me.com',first_name='admin',last_name='admin',
+            password='adminpass',user_type=User.Types.ADMINISTRATOR)
+        token = AuthToken.objects.create(user=admin_user)
+        return {
+            'user' : admin_user,
+            'token' : token[1]
+        }
 
+    def create_dummy_student(self):
+        """
+        returns a dictionary containing the student user data, account data and the token.
+        """
+        student_user = User.objects.create_user(email='student@me.com',first_name='student',last_name='student',
+            password='studentpass',user_type=User.Types.STUDENT)
+        student_account = Student.objects.create(user=student_user,section=self.section1,
+            registration_number='151638468951',group=1)
+        token = AuthToken.objects.create(user=student_user)
+        return {
+            'user' : student_user,
+            'account' : student_account,
+            'token' : token[1]
+        }
+
+    def create_dummy_teacher(self):
+        """
+        returns a dictionary containing the admin user data, account data and the token.
+        """
+        teacher_user = User.objects.create_user(email='teacher@me.com',first_name='teacher',last_name='teacher',
+            password='teacherpass',user_type=User.Types.TEACHER)
+        teacher_account = Teacher.objects.create(user=teacher_user,grade='MAB')
+        token = AuthToken.objects.create(user=teacher_user)
+        return {
+            'user' : teacher_user,
+            'account' : teacher_account,
+            'token' : token[1]
+        }
+
+    def create_dummy_superuser(self):
+        """
+        returns a dictionary containing the superuser user data and the token.
+        """
+        super_user = User.objects.create_superuser(email='superuser@me.com',first_name='superuser',last_name='superuser',
+        password='superuserpass')
+        token = AuthToken.objects.create(user=super_user)
+        return {
+            'user' : super_user,
+            'token' : token[1]
+        }
 
     def tearDown(self):
         return super().tearDown()
--- Accounts/tests/test_views.py
+++ Accounts/tests/test_views.py
@@ -36,7 +36,8 @@ class TestUserRegister(TestSetup):
         Ensures that a student cannot create a teacher account.
         """
         url = reverse('teacher_list')
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.student_user_token[1])
+        student = self.create_dummy_student()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + student['token'])
         result = self.client.post(url,self.teacher_data)
         self.assertEqual(result.status_code,status.HTTP_403_FORBIDDEN)
 
@@ -45,7 +46,8 @@ class TestUserRegister(TestSetup):
         Ensures that a teacher cannot create another teacher account.
         """
         url = reverse('teacher_list')
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.teacher_user_token[1])
+        teacher = self.create_dummy_teacher()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + teacher['token'])
         result = self.client.post(url,self.teacher_data)
         self.assertEqual(result.status_code,status.HTTP_403_FORBIDDEN)
 
@@ -54,7 +56,8 @@ class TestUserRegister(TestSetup):
         Ensures that an admin can create a teacher account.
         """
         url = reverse('teacher_list')
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.admin_user_token[1])
+        admin = self.create_dummy_admin()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + admin['token'])
         result = self.client.post(url,self.teacher_data)
         self.assertEqual(result.status_code,status.HTTP_201_CREATED)
 
@@ -81,7 +84,8 @@ class TestLogin(TestSetup):
         a student account credentials.
         """
         url = reverse('login')
-        credentials = {'email':self.student_user.email,'password':'studentpass'}
+        student = self.create_dummy_student()
+        credentials = {'email':student['user'].email,'password':'studentpass'}
         result = self.client.post(url,credentials)
         self.assertEqual(result.status_code,status.HTTP_201_CREATED)
 
@@ -91,7 +95,8 @@ class TestLogin(TestSetup):
         a teacher account credentials.
         """
         url = reverse('login')
-        credentials = {'email':self.teacher_user.email,'password':'teacherpass'}
+        teacher = self.create_dummy_teacher()
+        credentials = {'email':teacher['user'].email,'password':'teacherpass'}
         result = self.client.post(url,credentials)
         self.assertEqual(result.status_code,status.HTTP_201_CREATED)
 
@@ -100,6 +105,101 @@ class TestLogin(TestSetup):
         Ensures that logging in with a superuser is refused.
         """
         url = reverse('login')
-        credentials = {'email':self.super_user.email,'password':'superuserpass'}
+        superuser = self.create_dummy_superuser()
+        credentials = {'email':superuser['user'].email,'password':'superuserpass'}
         result = self.client.post(url,credentials)
         self.assertEqual(result.status_code,status.HTTP_400_BAD_REQUEST)
+
+class TestLogout(TestSetup):
+    """
+    Unit tests for logging out through the API.
+    """
+    def test_logout_no_credentials(self):
+        """
+        Ensures that logging out with no/wrong credentials is refused.
+        """
+        url = reverse('knox_logout')
+        #Test with no credentials
+        result = self.client.post(url)
+        self.assertEqual(result.status_code,status.HTTP_401_UNAUTHORIZED)
+        #Test with wrong credentials
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + 'wrongtoken')
+        result = self.client.post(url)
+        self.assertEqual(result.status_code,status.HTTP_401_UNAUTHORIZED)
+    
+    def test_logout_student(self):
+        """
+        Ensures that a student is able to logout.
+        """
+        url = reverse('knox_logout')
+        student = self.create_dummy_student()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + student['token'])
+        result = self.client.post(url)
+        self.assertEqual(result.status_code,status.HTTP_204_NO_CONTENT)
+    
+    def test_logout_teacher(self):
+        """
+        Ensures that a teacher is able to logout.
+        """
+        url = reverse('knox_logout')
+        teacher = self.create_dummy_teacher()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + teacher['token'])
+        result = self.client.post(url)
+        self.assertEqual(result.status_code,status.HTTP_204_NO_CONTENT)
+    
+    def test_logout_admin(self):
+        """
+        Ensures that an admin is able to logout.
+        """
+        url = reverse('knox_logout')
+        admin = self.create_dummy_admin()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + admin['token'])
+        result = self.client.post(url)
+        self.assertEqual(result.status_code,status.HTTP_204_NO_CONTENT)
+
+class TestLogoutAll(TestSetup):
+    """
+    Unit tests for logging out all tokens through the API.
+    """
+    def test_logout_all_no_credentials(self):
+        """
+        Ensures that logging out with no/wrong credentials is refused.
+        """
+        url = reverse('knox_logout_all')
+        #Test with no credentials
+        result = self.client.post(url)
+        self.assertEqual(result.status_code,status.HTTP_401_UNAUTHORIZED)
+        #Test with wrong credentials
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + 'wrongtoken')
+        result = self.client.post(url)
+        self.assertEqual(result.status_code,status.HTTP_401_UNAUTHORIZED)
+    
+    def test_logout_all_student(self):
+        """
+        Ensures that a student is able to logout all tokens.
+        """
+        url = reverse('knox_logout')
+        student = self.create_dummy_student()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + student['token'])
+        result = self.client.post(url)
+        self.assertEqual(result.status_code,status.HTTP_204_NO_CONTENT)
+    
+    def test_logout_teacher(self):
+        """
+        Ensures that a teacher is able to logout all tokens.
+        """
+        url = reverse('knox_logout_all')
+        teacher = self.create_dummy_teacher()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + teacher['token'])
+        result = self.client.post(url)
+        self.assertEqual(result.status_code,status.HTTP_204_NO_CONTENT)
+    
+    def test_logout_admin(self):
+        """
+        Ensures that an admin is able to logout all tokens.
+        """
+        url = reverse('knox_logout_all')
+        admin = self.create_dummy_admin()
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + admin['token'])
+        result = self.client.post(url)
+        self.assertEqual(result.status_code,status.HTTP_204_NO_CONTENT)
