af8a6723d21d315d4f6c879e1d63d206b136a42b
Alphashow <lastblast@outlook.fr>
2021-04-25 11:12:10 +0100

    Implemented unit tests for app version support check.

--- External/tests/test_setup.py
+++ External/tests/test_setup.py
@@ -1,4 +1,11 @@
 from rest_framework.test import APITestCase
+from External.models import AppVersionSupport
+class TestSetup(APITestCase):
 
-
-
+    def setUp(self):
+        #Create supported and unsupported versions
+        AppVersionSupport.objects.create(version='V0.1',is_supported=False)
+        AppVersionSupport.objects.create(version='V0.2',is_supported=False)
+        AppVersionSupport.objects.create(version='V0.3',is_supported=True)
+        AppVersionSupport.objects.create(version='V0.4',is_supported=True)
+        return super().setUp()
