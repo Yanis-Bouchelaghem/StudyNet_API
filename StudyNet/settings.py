d980393fcc7a3cc20b8a616d49739bc53b311c15
Alphashow <lastblast@outlook.fr>
2021-04-26 12:35:32 +0100

    Created Procfile & runtime, configured allowed hosts.

--- StudyNet/settings.py
+++ StudyNet/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG',default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['study-net-api.herokuapp.com','127.0.0.1']
 
 
 # Application definition
