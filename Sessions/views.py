ba51ec3f5e0996fc48fe9ddb55f82f91e548d5af
Alphashow <lastblast@outlook.fr>
2021-05-08 11:59:12 +0100

    Implemented sessions retrieval.

--- Sessions/views.py
+++ Sessions/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
 
+from .models import Session
+from .serializers import SessionSerializer
 # Create your views here.
+
+class SessionList(APIView):
+    """
+    Retrieves the list of sessions
+    """
+    #TODO: Remove empty permission classes when done developing.
+    permission_classes = []
+
+    def get(self,request):
+        sessions = Session.objects.all()
+        seriliazer = SessionSerializer(sessions, many=True)
+        return Response(seriliazer.data)
