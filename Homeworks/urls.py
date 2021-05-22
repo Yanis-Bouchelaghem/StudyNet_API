e8c6b7d1a970ed0564e9104851e16f1406325a16
Alphashow <lastblast@outlook.fr>
2021-05-22 13:36:07 +0100

    Implemented specific homework retrieve, update and delete.

--- Homeworks/urls.py
+++ Homeworks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import HomeworkList
+from .views import HomeworkDetail, HomeworkList
 
 urlpatterns = [
     path('homeworks/',HomeworkList.as_view(), name='homework_list'),
+    path('homeworks/<int:pk>/', HomeworkDetail.as_view(), name='homework_detail'),
 ]
--- Homeworks/views.py
+++ Homeworks/views.py
@@ -1,3 +1,4 @@
+from django.http.response import Http404
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import serializers, status
@@ -34,3 +35,42 @@ class HomeworkList(APIView):
             return Response(serializer.data,status=status.HTTP_201_CREATED)
         else:
             return Response({"Unauthorized":"Only teachers may create homeworks."},status=status.HTTP_401_UNAUTHORIZED)
+
+class HomeworkDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Homework.objects.get(pk=pk)
+        except:
+            raise Http404
+    
+    def get(self,request, pk):
+        homework = self.get_object(pk)
+        serializer = HomeworkSerializer(homework, context={'teacher_id':request.user.id})
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        homework = self.get_object(pk)
+        if request.user.user_type == User.Types.TEACHER:
+            if homework.assignment.teacher_section.teacher.user.id == request.user.id:
+                serializer = HomeworkSerializer(homework, data=request.data, context={'teacher_id':request.user.id})
+                serializer.is_valid(raise_exception=True)
+                serializer.save()
+                return Response(serializer.data)
+            else:
+                return Response({'Unauthorized':'You can only update your own homeworks.'},status=status.HTTP_401_UNAUTHORIZED)
+        else:
+            return Response({'Unauthorized':'Only teachers can update their homeworks.'},status=status.HTTP_401_UNAUTHORIZED)
+    
+    def delete(self, request, pk):
+        homework = self.get_object(pk)
+        #Check that this user is a teacher
+        if request.user.user_type == User.Types.TEACHER:
+            #Check that this homework has been created by this teacher
+            if homework.assignment.teacher_section.teacher.user.id == request.user.id:
+                homework.delete()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+            else:
+                return Response({'Unauthorized':'You can only delete your own homeworks.'}, status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({'Unauthorized':'Only teachers can delete their homeworks.'}, status=status.HTTP_401_UNAUTHORIZED)
