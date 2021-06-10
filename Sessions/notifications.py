cf52b240a96ff4678ff79d34cd1470bc2fee686f
Alphashow <lastblast@outlook.fr>
2021-06-10 18:56:56 +0100

    Implemented better notifications for session updates.

--- Sessions/notifications.py
+++ Sessions/notifications.py
@@ -1,4 +1,5 @@
 from fcm_django.fcm import fcm_send_topic_message
+from .models import Session
 
 def notifySessionCreated(session):
     """
@@ -17,17 +18,38 @@ def notifySessionCreated(session):
         +"\nEnd time : " + end_time),
         message_title="New session created by teacher "+ teacher_name +".")
 
-def notifySessionUpdated(session):
+def notifySessionUpdated(oldSession, newSession):
     """
     Sends a push notification to the section about the update of the given session.
     """
-    section_code = session.assignment.teacher_section.section.code
-    teacher_name = session.assignment.teacher_section.teacher.user.last_name
-    module_name = session.assignment.module_section.module.name
+    #Figure out which fields were modified and add them to a string.
+    modified_fields = ""
+    if oldSession.assignment.module_section.module.code != newSession.assignment.module_section.module.code:
+        modified_fields += "Module : " + newSession.assignment.module_section.module.name + "\n"
+    if oldSession.day != newSession.day:
+        modified_fields += "Day : " + dict(Session.DaysOfWeek.choices)[newSession.day] + "\n"
+    if oldSession.start_time != newSession.start_time:
+        modified_fields += "Start time : " + str(newSession.start_time) + "\n"
+    if oldSession.end_time != newSession.end_time:
+        modified_fields += "End time : " + str(newSession.end_time) + "\n"
+    if oldSession.concerned_groups != newSession.concerned_groups:
+        modified_fields += "Concerned groups : " + str(newSession.concerned_groups) + "\n"
+    if oldSession.meeting_link != newSession.meeting_link:
+        modified_fields += "Meeting link : " + newSession.meeting_link + "\n"
+    if oldSession.meeting_number != newSession.meeting_number:
+        modified_fields += "Meeting number : " + newSession.meeting_number + "\n"
+    if oldSession.meeting_password != newSession.meeting_password:
+        modified_fields += "Meeting password : " + newSession.meeting_password + "\n"
+    if oldSession.comment != newSession.comment:
+        modified_fields += "Comment : " + newSession.comment + "\n"
+    
+    #Send the notification using the constructed string.
+    section_code = newSession.assignment.teacher_section.section.code
+    teacher_name = newSession.assignment.teacher_section.teacher.user.last_name
     fcm_send_topic_message(
         topic_name=section_code.replace(' ','_'),
-        message_body=("Teacher "+ teacher_name + " updated a session for the module \""
-        + module_name + "\""),
+        message_body=("Teacher "+ teacher_name + " updated a session.\nModified information:\n"
+        + modified_fields),
         message_title="Session updated by teacher "+ teacher_name +".")
 
 def notifySessionDeleted(session):
