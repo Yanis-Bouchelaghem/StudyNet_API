00f95f3d0963e7c074351470aa292cd70a607b9a
Alphashow <lastblast@outlook.fr>
2021-06-17 19:20:10 +0100

    Implemented teacher FCM token register and unregister.

--- External/serializers.py
+++ External/serializers.py
@@ -6,3 +6,8 @@ class AppVersionSerializer(serializers.Serializer):
     version = serializers.CharField(max_length=30)
     class Meta:
         fields = ['version',]
+
+class FCMTokenSerializer(serializers.Serializer):
+    FCM_token = serializers.CharField()
+    class Meta:
+        fields = ['FCM_token',]
--- External/views.py
+++ External/views.py
@@ -1,10 +1,14 @@
+from Accounts import serializers
+from django.db.models.signals import pre_delete
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 from fcm_django.fcm import fcm_send_topic_message
+from fcm_django.models import FCMDevice
 
-from .serializers import AppVersionSerializer
+from .serializers import AppVersionSerializer, FCMTokenSerializer
 from .models import AppVersionSupport
+from Accounts.models import User
 # Create your views here.
 
 class AppVersionCheckView(APIView):
@@ -32,3 +36,43 @@ class TestFCM(APIView):
     def post(self, request):
         fcm_send_topic_message(topic_name='ISIL-B-L3', message_body='Hello', message_title='A message')
         return Response()
+
+class RegisterTeacherFCM(APIView):
+
+    def post(self, request):
+        if request.user.user_type == User.Types.TEACHER:
+            serializer = FCMTokenSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            try:
+                #Check if his FCM token already exists
+                existing = FCMDevice.objects.get(registration_id=serializer.data['FCM_token'])
+                #It already exists, delete it
+                existing.delete()
+            except:
+                #It doesn't exist, catch the exception and continue
+                pass
+            #Register the FCM token for this teacher
+            FCMDevice.objects.create(registration_id=serializer.data['FCM_token'],user=request.user,type='android')
+            return Response(status = status.HTTP_200_OK)
+        else:
+            return Response({'invalid_user_type':'Only teachers can subscribe to individual notifications.'},
+            status = status.HTTP_404_NOT_FOUND)
+
+class UnregisterTeacherFCM(APIView):
+
+     def post(self, request):
+        if request.user.user_type == User.Types.TEACHER:
+            serializer = FCMTokenSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            #Delete the FCM token for this teacher
+            try:
+                fcm_device = FCMDevice.objects.get(registration_id=serializer.data['FCM_token'],user=request.user)
+                fcm_device.delete()
+                return Response(status = status.HTTP_200_OK)
+            except FCMDevice.DoesNotExist:
+                return Response(status = status.HTTP_404_NOT_FOUND)
+                
+
+        else:
+            return Response({'invalid_user_type':'Only teachers can subscribe to individual notifications.'},
+            status = status.HTTP_404_NOT_FOUND)
